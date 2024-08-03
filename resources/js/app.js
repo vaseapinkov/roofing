cdab6b9229866b83795bbf32150eaf88e9c68b93
vaseapinkov <wasea.pinkov@gmail.com>
2024-08-04 01:52:19 +0300

    Add hero slider

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+
+import Swiper from "swiper/bundle";
+import 'swiper/css/bundle';
+
+new Swiper('#home-swiper', {
+    loop: true,
+    slidesPerView: 5,
+    centeredSlides: true,
+    speed: 1000,
+    allowTouchMove: false,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+});
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 
     <title>{{ $title ?? 'Page Title' }}</title>
 </head>

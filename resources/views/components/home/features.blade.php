65e709c72e20fdd77ab2ad3ac771599e075397a7
vaseapinkov <wasea.pinkov@gmail.com>
2024-08-05 07:04:40 +0300

    Remove todo in features

--- resources/views/components/home/features.blade.php
+++ resources/views/components/home/features.blade.php
@@ -1,18 +1,17 @@
 <?php
-// TODO: Replace with features
-$services = \App\Models\Feature::where('show_on_home_page', true)->get();
+$features = \App\Models\Feature::where('show_on_home_page', true)->get();
 ?>
 <section id="features" class="container mx-auto pt-[120px] pb-[90px]">
 
     <div class="flex place-content-center flex-wrap gap-x-4 gap-y-10">
-        @foreach($services as $service)
+        @foreach($features as $feature)
             <div class="w-[32.5%] group relative z-1 px-[50px] before:w-full before:bg-primary-100 before:h-[150px] before:absolute before:top-1/2 before:transform before:-translate-y-1/2 before:left-0 before:-z-10 before:rounded-lg">
                 <div class="relative flex flex-col h-full bg-white shadow-card px-[48px] pt-[45px] py-[50px] rounded-10 overflow-hidden">
                     <h4 class="text-xl text-gray-title font-heading font-bold mb-2">
-                        {{$service->name}}
+                        {{$feature->name}}
                     </h4>
                     <p class="text-gray-body mb-4">
-                        {{$service->home_page_description}}
+                        {{$feature->home_page_description}}
                     </p>
 
                     <a class="flex gap-4 items-center mt-auto text-gray-body group-hover:text-primary-500 transition duration-300">
@@ -25,7 +24,7 @@ $services = \App\Models\Feature::where('show_on_home_page', true)->get();
 
                     <div class="absolute w-[190px] h-[190px] rounded-full -bottom-[78px] -right-[58px] bg-primary-100">
                         <div class="absolute z-1 transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 w-[155px] h-[155px] rounded-full  p-[40px] bg-primary-400 group-hover:bg-primary-500 transition duration-300">
-                            <img class="group-hover:brightness-0 group-hover:invert transition duration-300" src="{{asset($service->home_page_icon)}}" alt="Icon for {{$service->name}}">
+                            <img class="group-hover:brightness-0 group-hover:invert transition duration-300" src="{{asset($feature->home_page_icon)}}" alt="Icon for {{$feature->name}}">
                         </div>
                     </div>
                 </div>

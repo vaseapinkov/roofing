96f744eff8b9f8cf478d1382964123d374740885
vaseapinkov <wasea.pinkov@gmail.com>
2024-08-09 07:46:17 +0300

    Add responsive about us section

--- resources/views/components/home/about-us.blade.php
+++ resources/views/components/home/about-us.blade.php
@@ -3,8 +3,8 @@ $features = ['32k Partners have worked us.', 'Professional and experienced human
 ?>
 <section id="about-us" class="container mx-auto pb-[120px]">
 
-    <div class="flex flex-col-reverse 2xl:flex-row">
-        <div class="w-full 2xl:w-[40%]">
+    <div class="flex flex-col-reverse lg:flex-row">
+        <div class="w-full xl:w-[40%]">
             <x-section-title :centred="false" title="We're committed to Roofing Excellence" sub-title="About Our Company"/>
             <p class="text-gray-body mb-4">There are many variations of passages of Lorem Ipsum available, but the majori have suffered alteration in some form, by injected humour, or randomised word which don't look even slightly believable.</p>
             <ul class="mb-8">
@@ -21,12 +21,12 @@ $features = ['32k Partners have worked us.', 'Professional and experienced human
 
             <x-button icon="arrow-right" class="w-fit">Learn more</x-button>
         </div>
-        <div class="w-full 2xl:w-[60%]">
-            <div class="flex justify-end items-center relative">
-                <img class="rounded-10 mb-12 2x:mb-0" src="{{Vite::asset('resources/images/about-us/img-1.jpg')}}" alt="About Us 1">
-                <img class="hidden 2xl:block rounded-10 ml-5" src="{{Vite::asset('resources/images/about-us/img-2.jpg')}}" alt="About Us 2">
+        <div class="w-full xl:w-[60%]">
+            <div class="flex justify-center xl:justify-end items-center relative">
+                <img class="rounded-10 mb-12 2xl:mb-0" src="{{Vite::asset('resources/images/about-us/img-1.jpg')}}" alt="About Us 1">
+                <img class="hidden sm:block lg:hidden xl:block rounded-10 ml-5" src="{{Vite::asset('resources/images/about-us/img-2.jpg')}}" alt="About Us 2">
 
-                <div class="hidden 2xl:block">
+                <div class="hidden lg:block">
                     <div class="absolute bg-white shadow-3xl p-5 flex items-center gap-5 rounded-10 left-[70px] bottom-[87px] w-[352px]">
                         <div class="bg-primary-500 rounded-5 h-[63px] w-[63px] flex items-center justify-center">
                             <img src="{{Vite::asset('resources/images/about-us/icon-1.svg')}}" alt="Icon">
